72f916b148fe43802a21bb9906a1e774e82121f3
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-08-22 03:42:41 +0100

    Buttons and quit msg box added

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from PyQt4 import QtGui
 from PyQt4.QtGui import QPalette
 from PyQt4.QtGui import QBrush
 from PyQt4.QtGui import QPixmap
+from PyQt4.QtGui import QToolTip
+from PyQt4.QtGui import QFont
 
 # My current intention is to have each window as a separate class.
 class Menu(QtGui.QWidget):
@@ -16,16 +18,35 @@ class Menu(QtGui.QWidget):
         self.initUI()
 
     def initUI(self):
-
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
         self.setGeometry(300, 300, 300, 300)
         self.setWindowTitle('World Builder')
         self.setWindowIcon(QtGui.QIcon('icon.png'))
         palette	= QPalette()
         palette.setBrush(QPalette.Background,QBrush(QPixmap("bg.jpg")))
+        btn_create_world = QtGui.QPushButton('Create World', self)
+        btn_load_world = QtGui.QPushButton('Load World', self)
+        btn_create_world.setToolTip('This creates a new world for aRPG MUD.')
+        btn_load_world.setToolTip('This loads an existing world for aRPG MUD.')
+        btn_create_world.resize(btn_create_world.sizeHint())
+        btn_load_world.resize(btn_load_world.sizeHint())
+        btn_create_world.move(50, 50)
+        btn_load_world.move(50, 100)
+
 
         self.setPalette(palette)
 
         self.show()
+    def closeEvent(self, event):
+
+        reply = QtGui.QMessageBox.question(self, 'Message',
+            "Are you sure to quit?", QtGui.QMessageBox.Yes |
+            QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 def main():
 
