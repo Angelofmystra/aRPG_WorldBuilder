1824f9a5809f43e5d914b539817ec2737024df80
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-08-22 03:03:58 +0100

    updated window size and title

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-"""
-ZetCode PyQt4 tutorial
-
-In this example, we create a simple
-window in PyQt4.
-
-author: Jan Bodnar
-website: zetcode.com
-last edited: October 2011
-"""
-
 import sys
 from PyQt4 import QtGui
 
@@ -22,8 +11,8 @@ def main():
 
     w = QtGui.QWidget()
     w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
+    w.move(800, 600)
+    w.setWindowTitle('World Builder')
     w.show()
 
     sys.exit(app.exec_())
